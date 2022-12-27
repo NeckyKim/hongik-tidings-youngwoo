15a5b72693801ed62d3aa03caa786cdb56215b65
NeckyKim <kyeryoong@gmail.com>
2022-12-27 19:29:02 +0900

    Home page was modified. (221227)

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
@@ -10,11 +9,45 @@ import styles from './Home.module.css';
 
 
 export default function Home() {
+    const [width, setWidth] = useState<number>(window.innerWidth);
+
+
+    useEffect(() => {
+        window.addEventListener("resize", () => { setWidth(window.innerWidth); })
+    }, [])
+
     let keywords: string[][] = [
-        ["도전", "challenge", "挑戰"],
-        ["노력", "effort", "努力"],
-        ["성장", "growth", "成長"]
+        ["도전", "challenge", "挑戰", "RubyMine"],
+        ["노력", "effort", "努力", "GoLand"],
+        ["성장", "growth", "成長", "Kotlin"]
     ]
+
+
+
+    function KeywordsContainer({ index }: { index: number }) {
+        return (
+            <div className={styles.container}>
+                <img className={styles.background} src={process.env.PUBLIC_URL + "/backgrounds/" + keywords[index][3] + ".png"} />
+
+                <div className={styles.textContainer}>
+                    <div className={styles.textKorean}>
+                        {keywords[index][0]}
+                    </div>
+
+                    <div className={styles.textHanja}>
+                        {keywords[index][2]}
+                    </div>
+                </div>
+
+                <div
+                    className={styles.textEnglish}
+                    style={keywords[index][1] === "challenge" ? { fontSize: "min(20rem, 22vw)" } : { fontSize: "min(20rem, 30vw)" }}
+                >
+                    {keywords[index][1]}
+                </div>
+            </div>
+        )
+    }
 
 
 
@@ -35,62 +68,18 @@ export default function Home() {
     return (
         <div>
             <div className={styles.textTop}>
-                안녕하세요. 저는
-            </div>
-
-            <div className={styles.textBottom}>
-                하는 개발자 입니다.
+                안녕하세요. 저는,
             </div>
 
             <Slider {...settings} className={styles.slider}>
-                <div className={styles.backgroundContainer}>
-                    <img className={styles.background} src={process.env.PUBLIC_URL + "/backgrounds/RubyMine.png"} />
-
-                    <div className={styles.korean}>
-                        {keywords[0][0]}
-                    </div>
-
-                    <div className={styles.hanja}>
-                        {keywords[0][2]}
-                    </div>
-
-                    <div className={styles.english}>
-                        {keywords[0][1]}
-                    </div>
-                </div>
-
-                <div className={styles.backgroundContainer}>
-                    <img className={styles.background} src={process.env.PUBLIC_URL + "/backgrounds/GoLand.png"} />
-
-                    <div className={styles.korean}>
-                        {keywords[1][0]}
-                    </div>
-
-                    <div className={styles.hanja}>
-                        {keywords[1][2]}
-                    </div>
-
-                    <div className={styles.english}>
-                        {keywords[1][1]}
-                    </div>
-                </div>
-
-                <div className={styles.backgroundContainer}>
-                    <img className={styles.background} src={process.env.PUBLIC_URL + "/backgrounds/Kotlin.png"} />
-
-                    <div className={styles.korean}>
-                        {keywords[2][0]}
-                    </div>
-
-                    <div className={styles.hanja}>
-                        {keywords[2][2]}
-                    </div>
-
-                    <div className={styles.english}>
-                        {keywords[2][1]}
-                    </div>
-                </div>
+                <KeywordsContainer index={0} />
+                <KeywordsContainer index={1} />
+                <KeywordsContainer index={2} />
             </Slider>
+
+            <div className={styles.textBottom}>
+                하는 개발자 김영우 입니다.
+            </div>
         </div>
     )
 }
